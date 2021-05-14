f8ae4456a3d17ba96760fc780f2ff642b9242748
mattiolichiara <84195637+mattiolichiara@users.noreply.github.com>
2021-05-14 21:47:12 +0200

    Update ProceduralMaze.java

--- src/AI_MAZE/ProceduralMaze.java
+++ src/AI_MAZE/ProceduralMaze.java
@@ -11,11 +11,3 @@ public class ProceduralMaze {
     }
     
 }
-
-
-//fitting solution is if the piece is not in a blocked position, it can go up only if that upper cell hasn't been visited
-//depth first search anche per il player, perchè se rimane bloccato e gli unici muri liberi sono di una cella già visitata non deve essere passato
-//alla prossima generazione
-
-//movimento: ogni movimento deve essere randomico ma verso i muri liberi e mai visitati, se a metà percorso e a fine generazione si trova verso muri
-//liberi passa all'altra generazione, però ci deve sta la cazzo di mutazione che mi mette un botto di dubbi
